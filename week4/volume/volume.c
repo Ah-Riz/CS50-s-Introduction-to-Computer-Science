0226f87d0837eb75be87a5b561f40d78e50a40fb
Ahmad Rizki Maulana <ahmadrizkimaulana666@gmail.com>
2025-05-27 09:49:16 +0700

    done

--- week4/volume/volume.c
+++ week4/volume/volume.c
@@ -4,6 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int copy_header(FILE *input, FILE *output);
+int copy_samples(FILE *input, FILE *output, float factor);
+
 // Number of bytes in .wav header
 const int HEADER_SIZE = 44;
 
@@ -34,10 +37,49 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
+    copy_header(input, output);
 
     // TODO: Read samples from input file and write updated data to output file
+    copy_samples(input, output, factor);
 
     // Close files
     fclose(input);
     fclose(output);
+}
+
+int copy_header(FILE *input, FILE *output)
+{
+    // Read header from input file
+    int8_t header[HEADER_SIZE];
+    if (fread(header, HEADER_SIZE, 1, input) <= 0)
+    {
+        printf("Could not read header.\n");
+        return 2;
+    }
+
+    // Write header to output file
+    if (fwrite(header,  HEADER_SIZE, 1, output) <= 0)
+    {
+        printf("Could not write header.\n");
+        return 3;
+    }
+
+    return 0;
+}
+
+int copy_samples(FILE *input, FILE *output, float factor)
+{
+    int16_t sample;
+    while (fread(&sample, sizeof(int16_t), 1, input))
+    {
+        // Scale sample by factor
+        sample = (int16_t)(sample * factor);
+
+        if(fwrite(&sample, sizeof(int16_t), 1, output) <= 0)
+        {
+            printf("Could not write sample.\n");
+            return 4;
+        }
+    }
+    return 0;
 }
