2b22abe0690b6c58f8d78c54e1ffc720ec8231c5
Ahmad Rizki Maulana <ahmadrizkimaulana666@gmail.com>
2025-06-04 06:42:31 +0700

    done

--- week9/birthdays/app.py
+++ week9/birthdays/app.py
@@ -27,13 +27,33 @@ def index():
     if request.method == "POST":
 
         # TODO: Add the user's entry into the database
+        name = request.form.get("name")
+        month = request.form.get("month")
+        day = request.form.get("day")
+        db.execute("INSERT INTO birthdays (name, month, day) VALUES(?, ?, ?)", name, month, day)
 
         return redirect("/")
 
     else:
 
         # TODO: Display the entries in the database on index.html
-
-        return render_template("index.html")
+        months = {
+            1: "January",
+            2: "February",
+            3: "March",
+            4: "April",
+            5: "May",
+            6: "June",
+            7: "July",
+            8: "August",
+            9: "September",
+            10: "October",
+            11: "November",
+            12: "December"
+        }
+        birthdays = db.execute("SELECT id, name, month, day FROM birthdays")
+        for birthday in birthdays:
+            birthday["month"] = months[birthday["month"]]
+        return render_template("index.html", birthdays=birthdays)
 
 
